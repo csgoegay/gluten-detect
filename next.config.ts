75737fd9d30308585357e6745b1141406205c790
[dyad] <git@dyad.sh>
2025-11-14 13:17:41 +0000

    [dyad] Configuring image domains - wrote 1 file(s)

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,16 @@ const nextConfig: NextConfig = {
     }
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/aida-public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
